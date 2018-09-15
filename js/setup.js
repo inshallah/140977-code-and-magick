23bf4c09306464a3b92b0ddf481badf771b0d960
Musalaeva Larisa <musssss.94@mail.ru>
2018-09-15 23:47:46 +0300

    Добавлена программа отрисовки магов

---
+++ js/setup.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var userDialogElement = document.querySelector('.setup');
+userDialogElement.classList.remove('hidden');
+
+var similarListElement = userDialogElement.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+.content
+.querySelector('.setup-similar-item');
+
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', hhhHHhhh'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COATCOLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYESCOLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var wizards = [];
+
+
+var getRandomInt = function (min, max) {
+  return min + Math.floor(Math.random() * (max + 1 - min));
+};
+
+// Функция создания объекта
+
+var getWizard = function () {
+  return {
+    name: WIZARD_NAMES[getRandomInt(0, WIZARD_NAMES.length - 1)] + ' ' + WIZARD_SURNAMES[getRandomInt(0, WIZARD_SURNAMES.length - 1)],
+    coatColor: WIZARD_COATCOLOR[getRandomInt(0, WIZARD_COATCOLOR.length - 1)],
+    eyesColor: WIZARD_EYESCOLOR[getRandomInt(0, WIZARD_EYESCOLOR.length - 1)]
+  };
+};
+
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard['name'];
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard['coatColor'];
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard['eyesColor'];
+
+  return wizardElement;
+};
+
+var generateWizard = function () {
+  for (var i = 0; i < 4; i++) {
+    wizards.push(getWizard());
+  }
+}
+
+generateWizard();
+
+var render = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  similarListElement.appendChild(fragment);
+
+  userDialogElement.querySelector('.setup-similar').classList.remove('hidden');
+
+};
+
+render();
+
+
