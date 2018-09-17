6f8bcc1e1c742cc2081bc847a4080c133edd848e
Musalaeva Larisa <musssss.94@mail.ru>
2018-09-17 10:24:41 +0300

    Правка операторов

--- js/setup.js
+++ js/setup.js
@@ -10,7 +10,7 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
 .querySelector('.setup-similar-item');
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', hhhHHhhh'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COATCOLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYESCOLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
@@ -46,7 +46,7 @@ var generateWizard = function () {
   for (var i = 0; i < 4; i++) {
     wizards.push(getWizard());
   }
-}
+};
 
 generateWizard();
 
